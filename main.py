255071474f87d44bc08041d320b4d1f2136f0b94
gelila2718 <151121025+gelila2718@users.noreply.github.com>
2024-02-11 13:37:17 +0000

    final work

--- main.py
+++ main.py
@@ -1,44 +1,115 @@
 import requests
-import time
 import telebot
 from bs4 import BeautifulSoup
-from tabulate import tabulate  # Added import for tabulate
+from tabulate import tabulate
+from PIL import Image
+from io import BytesIO
+import random
 
-telegram_bot_token = '6950136624:AAHloC-eobxzHQWFjJycAF3qJNQo33pOjVM'
+telegram_bot_token = '6924678579:AAHnmzqbC1XsRrGvhg1sqY1H2eiKubENWu0'
+channel_id = '-1002131859663'  # Update this with your channel username or ID
+
 bot = telebot.TeleBot(telegram_bot_token)
-telegram_channel_username = '@WebScraped_Gelila_bot'
 
-# Dictionary to keep track of users who have opened the bot for the first time
-first_time_users = {}
+# Define categories for options
+categories = ['Fashion', 'Property', 'Electronics', 'Vehicles']
 
-@bot.message_handler(commands=['start'])
-def get_update(message):
-    user_id = message.from_user.id
+# Create ReplyKeyboardMarkup with categories as options
+keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+keyboard.add(
+    *[telebot.types.KeyboardButton(category) for category in categories])
 
-    if user_id not in first_time_users:
-        # User is opening the bot for the first time
-        first_time_users[user_id] = True
-        bot.send_message(message.chat.id, f"Welcome, {message.from_user.first_name}! This is the Web Scraping Bot. Type /start to interact.")
-    else:
-        # User has opened the bot before
-        bot.send_message(message.chat.id, "Welcome back!")
 
-    # Fetch and send job titles (limit to 10)
-    response = requests.get("https://www.booksiteethiopia.com")
+def scrape_website(url, category):
+  response = requests.get(url)
+  if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
-    results = soup.find(id='content')
+    if category.lower() == 'fashion':
+      fashion_list = soup.find_all('div', class_='masonry-item')[:8]
+      random.shuffle(fashion_list)
+      fashion_data = [(item.find('img')['src'], item.text.strip())
+                      for item in fashion_list]
+      return fashion_data
+    elif category.lower() == 'property':
+      property_list = soup.find_all('div', class_='masonry-item')[:8]
+      random.shuffle(property_list)
+      property_data = [(item.find('img')['src'], item.text.strip())
+                       for item in property_list]
+      return property_data
+    elif category.lower() == 'electronics':
+      electronics_list = soup.find_all('div', class_='masonry-item')[:8]
+      random.shuffle(electronics_list)
 
-    if results:
-        new_data = results.find_all('li', class_='product')  # Corrected variable name
-        new_data = new_data[:10]
+      electronics_data = [(item.find('img')['src'], item.text.strip())
+                          for item in electronics_list]
+      return electronics_data
+    elif category.lower() == 'vehicles':
+      vehicles_list = soup.find_all('div', class_='masonry-item')[:8]
+      random.shuffle(vehicles_list)
+      vehicles_data = [(item.find('img')['src'], item.text.strip())
+                       for item in vehicles_list]
+      return vehicles_data
+  else:
+    print(f"Failed to fetch data from {url}")
+    return []
 
-        if new_data:
-            new_data_list = [job.text for job in new_data]
-            job_table = tabulate([(index + 1, title) for index, title in enumerate(new_data_list)], headers=['No.', 'book title price and rating'], tablefmt='grid')
-            bot.send_message(message.chat.id, f"top 10 books from etehiopian site :\n{job_table}")
-        else:
-            bot.send_message(message.chat.id, "No updates found.")
+
+def send_to_channel(message):
+  max_length = 4000
+  if len(message) <= max_length:
+    bot.send_message(channel_id, message)
+  else:
+    chunks = [
+        message[i:i + max_length] for i in range(0, len(message), max_length)
+    ]
+    for chunk in chunks:
+      bot.send_message(channel_id, chunk)
+
+
+def send_images_with_text(images_data):
+  for img_url, text in images_data:
+    try:
+      response = requests.get(img_url)
+      img_data = response.content
+      img = Image.open(BytesIO(img_data))
+      # Convert image to bytes for sending it through Telegram
+      img_byte_array = BytesIO()
+      img.save(img_byte_array, format='PNG')
+      img_byte_array.seek(0)
+      # Send image with text
+      bot.send_photo(channel_id, img_byte_array, caption=text)
+    except Exception as e:
+      print(f"Error displaying image from {img_url}: {e}")
+
+
+@bot.message_handler(func=lambda message: True)
+def select_category(message):
+  if message.text in categories:
+    category = message.text
+    send_to_channel(f"New updates in {category}:")
+
+    if category.lower() in ['fashion', 'property', 'electronics', 'vehicles']:
+      if category.lower() == 'fashion':
+        data = scrape_website('https://jiji.com.et/clothing', category)
+      elif category.lower() == 'property':
+        data = scrape_website('https://jiji.com.et/new-builds', category)
+      elif category.lower() == 'electronics':
+        data = scrape_website(
+            'https://jiji.com.et/accessories-and-supplies-for-electronics',
+            category)
+      elif category.lower() == 'vehicles':
+        data = scrape_website('https://jiji.com.et/cars', category)
+
+      if data:
+        send_images_with_text(data)
+      else:
+        send_to_channel(f"No {category} updates found.")
     else:
-        bot.send_message(message.chat.id, "Unable to fetch data from the website.")
+      send_to_channel("Invalid category.")
+  else:
+    bot.send_message(message.chat.id,
+                     "Please select a category:",
+                     reply_markup=keyboard)
 
-bot.polling()
+
+bot.polling(True)
